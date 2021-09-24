f78115c606da49140ddd43f8b3744274881052ad
Nickolay Polyakov <nickolay.pol@gmail.com>
2021-09-24 20:09:18 +0300

    change description meta to english

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="Сайт проекта Виртуальный Старый Саратов"
+                    content="Site of the project Old Saratov in VR"
                 />
                 <meta
                     property="og:image"
